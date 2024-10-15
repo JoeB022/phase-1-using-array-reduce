c9b4d0d23302732c8b381b8f7c5fa39ded09977d
Brian Joe Kamau <joebrian998@gmail.com>
2024-10-15 10:49:54 +0300

    vocalized files

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+// Use reduce() to calculate the total number of batteries
+const totalBatteries = batteryBatches.reduce((total, batch) => {
+  return total + batch;
+}, 0); // Start with an initial total of 0
+
+// Log the total number of batteries
+console.log(totalBatteries); // Output the result
